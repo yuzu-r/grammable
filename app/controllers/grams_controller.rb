509c4403d8cfb8f29afe76778007cb33fc5c1ac4
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-05-11 15:23:41 +0000

    gram create

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,4 +5,19 @@ class GramsController < ApplicationController
   def new
     @gram = Gram.new   # comment this line to make an error that tests don't catch
   end
+
+  def create
+    @gram = Gram.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def gram_params
+      params.require(:gram).permit(:message)
+    end
+
 end
