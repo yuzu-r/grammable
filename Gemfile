b86e631ba176664b8469676b00ff4a41f0db76dc
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-05-11 14:45:23 +0000

    simple form for gram

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,8 @@
 class GramsController < ApplicationController
   def index
   end
+
+  def new
+    @gram = Gram.new   # comment this line to make an error that tests don't catch
+  end
 end
