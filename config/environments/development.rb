bf20b29d5d3482921a8dc24ef03ca1f65b8d6200
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-05-11 15:55:02 +0000

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Devise recommends:
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3030}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
